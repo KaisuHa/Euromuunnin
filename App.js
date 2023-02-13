8b2712a93cec5e1ce233b4b11b8705dfb9a51ec8
KaisuHa <kaisu_h@yahoo.com>
2023-02-13 13:16:52 +0200

    Tehtävä aloitettu

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
+import {Picker, useRef} from '@react-native-picker/picker';
+
+
 
 export default function App() {
+  const [rahamaara, setRahamaara] = useState();
+  const [selectedValue, setSelectedValue] = useState();
+  const [result, setResult] = useState('');
+  const [currency, setCurrency] = useState([]);
+
+  const data = [];
+  console.log(Object.keys(data));
+
+  var myHeaders = new Headers();
+myHeaders.append("apikey", "nlj3caeCrb6EWQwSiMzPKHEzbrEF3kfk");
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/exchangerates_data/convert?to={EUR}&from={from}&amount={amount}", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log(result)
+  .catch(error => console.log('error', error)));
+
+  //const pickerRef = useRef();
+
+  //function open() {
+ //   pickerRef.current.focus();
+ // }
+  
+ // function close() {
+ //   pickerRef.current.blur();
+ // }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{result} €</Text>
+      <TextInput style={{textDecorationLine: 'underline'}}
+      placeholder="amount"
+      keyboardType="numeric"
+      value={rahamaara}
+      //onChangeText {  } 
+      > </TextInput>
+      <Picker     
+      selectedValue={selectedValue}
+      onValueChange={(itemValue, itemIndex) =>
+        setSelectedValue(itemValue)
+        }>
+        <Picker.Item label="Valuutta" value="valuutta" />
+      </Picker>
+      
+      <Button title="CONVERT"  />
     </View>
   );
 }
